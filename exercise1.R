72e421e5baa03315fefd4f1032c04da27e24cd08
sabarishmogallapalli <65688211+sabarishmogallapalli@users.noreply.github.com>
2024-05-19 12:46:23 -0500

    Add files via upload

---
+++ exercise1.R
@@ -0,0 +1,49 @@
+# 1.1 Simple addition and subtraction
+
+10 + 5
+10 - 5
+
+# 1.2 An asterisk is used for multiplication
+
+10*1576
+
+# 1.3 A forward slash is used for division
+
+15760/10
+
+# 1.4 R has some values that are preset.
+
+10+pi
+
+10/pi
+
+# 1.5 R uses the carat (ˆ) for exponents
+
+10^2
+
+#1.6 R uses the less than symbol followed by the hyphen as an assignment operator. (The two symbols form an icon that looks like a left facing arrow.) R uses the # symbol for comments; anything you enter after a # will be ignored by the R processor.
+
+x <- 10
+x - 3
+
+#1.7 Beware of how R implements order in complex mathematical operations. The following expression correctly evaluates to 12. Can you explain why?
+
+x <- 10
+x - 3 + 10 / 2
+
+#1.8 If x <- 10 and x - 3 + 10 / 2 = 12 How would you rewrite the expression to = 8.5?
+
+x <- 10
+x - 3 + 3 / 2
+
+#1.9 R has some built-in mathematical functions:
+
+sqrt(12)
+abs(-23)
+round(pi)
+
+#1.10 R can easily create sequences of numbers:
+
+1:10
+12:37
+
